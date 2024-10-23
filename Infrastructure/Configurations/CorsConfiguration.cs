46bf82f476a151e14d2642f5db946cbfd18e3b77
meccar <tung14hn@gmail.com>
2024-10-23 14:59:08 +0700

    feature (configuration): implemented cors

---
+++ Infrastructure/Configurations/CorsConfiguration.cs
@@ -0,0 +1,20 @@
+namespace taf_server.Infrastructure.Configurations;
+
+public static class CorsConfiguration
+{
+    public static IServiceCollection ConfigureCors(this IServiceCollection services, string appCors)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(appCors, policy =>
+                {
+                    policy
+                        .WithOrigins("*")
+                        .AllowCredentials()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
+        return services;
+    }
+}
--- Infrastructure/InfrastructureServicesConfiguration.cs
+++ Infrastructure/InfrastructureServicesConfiguration.cs
@@ -8,13 +8,13 @@ public static class InfrastructureServicesConfiguration
         IConfiguration configuration, string appCors)
     {
         // services.ConfigureRedis(configuration);
-        // services.ConfigureCors(appCors);
         // services.ConfigureQuartz();
         // services.ConfigureHangfireServices();
         // services.ConfigureApplication();
         // services.ConfigureAppSettings(configuration);
         // services.ConfigureAuthetication();
         services.ConfigureDependencyInjection();
+        services.ConfigureCors(appCors);
         services.ConfigureMediatR();
         services.ConfigureSwagger();
         services.ConfigureDbContext(configuration);
